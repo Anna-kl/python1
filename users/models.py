abd2ef7dfe47b01ed8c1aafd85f3e77cd8a94bfb
Anna <klimova_88@mail.ru>
2022-03-12 13:06:35 +0300

    add models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    body = models.TextField()
+    dttm_add = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+
